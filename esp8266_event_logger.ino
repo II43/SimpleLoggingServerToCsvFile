db37f32cf1186378edca4216c8b92331e1fd9a57
LL2 <lukas.lansky@ricardo.com>
2020-01-02 15:58:50 +0100

    Initial software for ESP8266 using Adafruit IO

---
+++ esp8266_event_logger.ino
@@ -0,0 +1,89 @@
+// Adafruit IO Publish Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// this int will hold the current count for our sketch
+int count = 0;
+
+// set up the 'counter' feed
+AdafruitIO_Feed *counter = io.feed("PIR 47");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  //Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    //Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  //Serial.println();
+  //Serial.println(io.statusText());
+
+  // send the event
+  send_event(); 
+
+  // sleep until reset
+  ESP.deepSleep(0);
+
+}
+
+void loop()
+{
+  /* Empty - One shot application */
+}
+
+void send_event() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  //io.run();
+
+  // save count to the 'counter' feed on Adafruit IO
+  //Serial.print("sending -> ");
+  //Serial.println(count);
+  counter->save(count);
+  
+  // write data to AIO
+  io.run();
+  
+
+  // increment the count by 1
+  count++;
+
+  // Adafruit IO is rate limited for publishing, so a delay is required in
+  // between feed->save events. In this example, we will wait three seconds
+  // (1000 milliseconds == 1 second) during each loop.
+  delay(500);
+
+}
